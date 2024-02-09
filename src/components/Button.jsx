3f625bc485c4c2f1ce2294bf3109e3ea33617158
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-02-10 00:18:43 +0530

    css fixes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
-const Button = ({label, icon}) => {
+const Button = ({ label, icon, onClick }) => {
   return (
-    <button className="bg-red-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full flex items-center justify-center gap-2">
-         
-{label}
-{icon}
+    <button
+      onClick={onClick}
+      className="bg-red-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full flex items-center justify-center gap-2"
+    >
+      {label}
+      {icon}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/sections/Sidebar.jsx
+++ src/sections/Sidebar.jsx
@@ -3,7 +3,53 @@ import Button from "../components/Button";
 import { CiMap } from "react-icons/ci";
 import Card from "../components/Card";
 import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
 const Sidebar = ({ handleVisible }) => {
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
+
+  const loadScript = (src) => {
+    return new Promise((resovle) => {
+      const script = document.createElement("script");
+      script.src = src;
+
+      script.onload = () => {
+        resovle(true);
+      };
+
+      script.onerror = () => {
+        resovle(false);
+      };
+
+      document.body.appendChild(script);
+    });
+  };
+
+  const displayRazorpay = async () => {
+    try {
+      await loadScript("https://checkout.razorpay.com/v1/checkout.js");
+
+      const options = {
+        key: "rzp_test_0202rN36C3r3rq",
+        currency: "INR",
+        amount: 100 * 100,
+        name: "Trip Booking",
+        description: "Thanks for purchasing",
+
+        handler: async function (response) {
+          await response.razorpay_payment_id;
+          setPaymentSuccess(true);
+        },
+        prefill: {
+          name: "Trip Booking",
+        },
+      };
+
+      const paymentObject = new window.Razorpay(options);
+      paymentObject.open();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="bg-white md:w-[400px] w-full h-full md:rounded-r-3xl md:rounded-b-3xl pl-7 md:fixed md:top-0 md:left-0 md:z-20 max-md:mt-4">
       <div className="flex justify-between items-start">
@@ -36,6 +82,7 @@ const Sidebar = ({ handleVisible }) => {
       <div className="flex justify-start items-center gap-4 pl-4 pt-4 pb-6 animate-jump-in animate-duration-[2000ms] animate-delay-200 animate-ease-out">
         <Button label="See Destinations "></Button>
         <Button
+          onClick={displayRazorpay}
           label="Book Now"
           icon={<CiMap className="bg-transparent" />}
         ></Button>
